09f3dabcb19649241a2e32d2dbf930bbaa6094c4
Abdul Moqueet <36290215+Abdul-Moqueet@users.noreply.github.com>
2020-02-01 19:50:24 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,18 +3,7 @@ window.onload = function () {
     var date = new Date();
     document.getElementById("copyright").innerHTML = "&copy; Abdul Moqueet | All Right Reserved "+date.getFullYear()+".";
 
-    setTimeout(() => {
-
-        AOS.init({
-            easing: 'ease-out-back',
-            duration: 1200,
-            delay: 100,
-            mirror: true,
-            anchorPlacement: 'bottom-bottom',
-            offset: 160
-        });
-
-}, 100);
+    alert("hi");
 
 
 };
