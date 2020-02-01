e80a610dfd74fae26c19ec2c65755c8f19114e81
Abdul Moqueet <Abdul-Moqueet@users.noreply.github.com>
2020-02-01 19:43:58 +0530

    Added More Projects

--- src/main.js
+++ src/main.js
@@ -2,38 +2,20 @@ window.onload = function () {
 
     var date = new Date();
     document.getElementById("copyright").innerHTML = "&copy; Abdul Moqueet | All Right Reserved "+date.getFullYear()+".";
-    
-    let Sick_Line_Bro_0x2930 = [
-        'ease-out-back',
-        'bottom-bottom'
-    ];
-    
-    (function (_0x347f01, _0x5ae81d) {
-        
-        let _0x26ea9d = (_0x18b34c) => {
-            while (--_0x18b34c) {
-                _0x347f01['push'](_0x347f01['shift']());
-            }
-        };
-        
-        _0x26ea9d(++_0x5ae81d);
-        
-    }(Sick_Line_Bro_0x2930, 0x192));
-    
-    let Sick_Line_Bro_0x7a44 = (_0x1dcf84, _0x4a4ce2) => {
-        _0x1dcf84 = _0x1dcf84 - 0x0;
-        let _0x4f571c = Sick_Line_Bro_0x2930[_0x1dcf84];
-        return _0x4f571c;
-    };
-    
-    AOS['init']({
-        'easing': Sick_Line_Bro_0x7a44('0x0'),
-        'duration': 0x4b0,
-        'delay': 0x64,
-        'mirror': !![],
-        'anchorPlacement': Sick_Line_Bro_0x7a44('0x1'),
-        'offset': 0xa0
-    });
+
+    setTimeout(() => {
+
+        AOS.init({
+            easing: 'ease-out-back',
+            duration: 1200,
+            delay: 100,
+            mirror: true,
+            anchorPlacement: 'bottom-bottom',
+            offset: 160
+        });
+
+}, 100);
+
 
 };
 
