75e5b1165e67a5bb57878bd9faaea323628abf9c
Abdul_Moqueet <moqueetabdul@gmail.com>
2020-06-27 10:56:24 +0530

    scrollable navbar added

--- script/script.js
+++ script/script.js
@@ -1,5 +1,20 @@
 $(document).ready(() => {
 
+    $('#section-about-me').waypoint(() => $("#main-nav-container").toggleClass("main-nav-active"), {
+        offset: '100px'
+    });
+
+    $('#section-about-me').waypoint((direction) => {
+
+        if (direction === 'down')
+            $("#main-nav-container").css("top", "0");
+        else
+            $("#main-nav-container").css("top", "-80px");
+
+    }, {
+        offset: '55px'
+    });
+
     $("#to").floatingLabel();
     $("#sub").floatingLabel();
     $("#msg").floatingLabel();
