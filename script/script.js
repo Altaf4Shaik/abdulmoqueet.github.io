1ffbf40896803294c640a73e62aac911f495d61e
Abdul_Moqueet <moqueetabdul@gmail.com>
2020-06-29 21:01:22 +0530

    Nav-Section Heighlight complete for desktop and mobile

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    var currentSection = "";
+
     $('#about').waypoint(function () {
         $("#main-nav-container").toggleClass("main-nav-active");
     }, {
@@ -23,14 +25,30 @@ $(document).ready(function () {
     $("#msg").floatingLabel();
 
     $(".nav-burger").click(function () {
+
+        if(currentSection!="")
+            helightCurrentSection();
+
         $(".nav-burger .line-1, .nav-burger .line-3").toggleClass("nav-burger-active");
         $(".nav-burger .line-2").toggleClass("nav-burger-active-line-2");
-
         $(".mobile-nav ul").toggleClass("left-nav-peek");
+
+
+        function helightCurrentSection(){
+            
+            var mobileNavLinks = $(".mobile-nav .mob-nav");
+
+            mobileNavLinks.each(function(){
+                $(this).removeClass('nav-active');
+                if($(this).attr("href")=="#"+currentSection.attr('id'))
+                    $(this).addClass("nav-active");
+            });
+        }
+
     });
 
 
-    // Isotope Scritp--------------
+    // Isotope --------------
     var projectItems = $(".project-items"),
         projectFilter = $(".project-filter");
 
@@ -79,5 +97,55 @@ $(document).ready(function () {
 
         event.preventDefault();
     });
+
+    // Heighlight Nav Menus-------------------------------
+
+    var $navigationLinks = $('.desktop-nav > ul > li > a');
+    var $sections = $($(".section").get().reverse());
+
+    var sectionIdTonavigationLink = {};
+    $sections.each(function () {
+        var id = $(this).attr('id');
+        sectionIdTonavigationLink[id] = $('.desktop-nav > ul > li > a[href=\\#' + id + ']');
+    });
+
+    function throttle(fn, interval) {
+        var lastCall, timeoutId;
+        return function () {
+            var now = new Date().getTime();
+            if (lastCall && now < (lastCall + interval)) {
+                clearTimeout(timeoutId);
+                timeoutId = setTimeout(function () {
+                    lastCall = now;
+                    fn.call();
+                }, interval - (now - lastCall));
+            } else {
+                lastCall = now;
+                fn.call();
+            }
+        };
+    }
+
+    function highlightNavigation() {
+        var scrollPosition = $(window).scrollTop();
+
+        $sections.each(function () {
+            currentSection = $(this);
+            var sectionTop = currentSection.offset().top - 55;
+
+            if (scrollPosition >= sectionTop) {
+                var id = currentSection.attr('id');
+                var $navigationLink = sectionIdTonavigationLink[id];
+                if (!$navigationLink.hasClass('nav-active')) {
+                    $navigationLinks.removeClass('nav-active');
+                    $navigationLink.addClass('nav-active');
+                    console.log($navigationLink);
+                }
+                return false;
+            }
+        });
+    }
+
+    $(window).scroll(throttle(highlightNavigation, 100));
 
 });
